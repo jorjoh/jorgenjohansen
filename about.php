8ddcca5e39a9a0c2d8e3da87d3120e2a220adddb
Jørgen Johansen <post@jorgenjohansen.no>
2016-10-18 22:09:11 +0200

    Optimized menubar

--- about.php
+++ about.php
@@ -43,25 +43,22 @@
 
     <nav class="menuHandler">
         <ul>
-            <li><a href="index.php">Home</a></li>
+            <li><a href="index.php" id="nav">Home</a></li>
             <li>
-                <a href="#">Projects</a>
+                <a href="#" id="nav">Projects</a>
                 <ul class="test">
-                    <li><a href="chromeExtention.php">ChromeExtention</a></li>
-                    <li><a href="dotNetApplikasjoner.php">.NetApplikasjoner</a></li>
+                    <li><a href="chromeExtention.php" >ChromeExtention</a></li>
+                    <li><a href="dotNetApplikasjoner.php" >.NetApplikasjoner</a></li>
                 </ul>
             </li>
-
-            <li><a href="about.php">About</a>
+            <li><a href="about.php" id="nav">About</a>
                 <ul>
-                    <li id="contact"><a id="cv"
-                                        href="https://www.dropbox.com/s/mxffeb53tbc6t27/J%C3%B8rgen%20Johansen.pdf?dl=0"
-                                        target="_blank">CV</a>
-                    </li>
+                    <li id="contact" ><a id="cv"
+                                         href="https://www.dropbox.com/s/mxffeb53tbc6t27/J%C3%B8rgen%20Johansen.pdf?dl=0"
+                                         target="_blank">CV</a></li>
                 </ul>
             </li>
-            <li><a href="contactMe.php">Contact me</a></li>
-
+            <li><a href="contactMe.php" id="nav">Contact me</a></li>
         </ul>
     </nav>
 
--- dotNetApplikasjoner.php
+++ dotNetApplikasjoner.php
@@ -55,16 +55,15 @@ ob_start();
                 <li>
                     <a href="#" id="nav">Projects</a>
                     <ul class="test">
-                        <li><a href="chromeExtention.php" id="nav">ChromeExtention</a></li>
-                        <li><a href="dotNetApplikasjoner.php" id="nav">.NetApplikasjoner</a></li>
+                        <li><a href="chromeExtention.php" >ChromeExtention</a></li>
+                        <li><a href="dotNetApplikasjoner.php" >.NetApplikasjoner</a></li>
                     </ul>
                 </li>
-
                 <li><a href="about.php" id="nav">About</a>
                     <ul>
-                        <li id="contact" id="nav"><a id="cv"
-                                                     href="https://www.dropbox.com/s/mxffeb53tbc6t27/J%C3%B8rgen%20Johansen.pdf?dl=0"
-                                                     target="_blank">CV</a></li>
+                        <li id="contact" ><a id="cv"
+                                             href="https://www.dropbox.com/s/mxffeb53tbc6t27/J%C3%B8rgen%20Johansen.pdf?dl=0"
+                                             target="_blank">CV</a></li>
                     </ul>
                 </li>
                 <li><a href="contactMe.php" id="nav">Contact me</a></li>
